fb9944510fb4f7e9304154f0193df837f43b3001
vkirbiatev <vkirbyatev@mail.ru>
2022-10-17 15:04:43 +0600

    Add planning re-subscription on new blocks

--- src/services/cron.service.ts
+++ src/services/cron.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
 import { BlockchainTypeEnum } from '../infrastructure/config/enums/blockchain-type.enum';
@@ -7,6 +7,8 @@ import { BlockSubscriberService } from './block-subscriber.service';
 
 @Injectable()
 export class CronService {
+  private readonly logger = new Logger();
+
   constructor(
     private readonly blockSubscriberService: BlockSubscriberService,
   ) {}
@@ -26,5 +28,28 @@ export class CronService {
         await this.fetchMissedBlocks();
       }
     }
+  }
+
+  @Cron(CronExpression.EVERY_4_HOURS)
+  async restartSubscription() {
+    const { isSubscribed } = await this.blockSubscriberService.status();
+
+    if (!isSubscribed) {
+      return;
+    }
+
+    await this.blockSubscriberService.unsubscribe(null);
+
+    this.logger.log('Start planned subscription restart');
+    await this.sleep(30000);
+    this.logger.log('Start planned subscription restart');
+
+    await this.blockSubscriberService.subscribe();
+  }
+
+  private async sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
   }
 }
